039ef9c1d6f50fdb1c0113c0de66cc8eb62090ca
HP <sreekanta.csd@gmail.com>
2022-09-16 23:45:55 +0600

    google authentication added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from 'react';
 
 
 
 const auth = getAuth(app);
 
+
 function App() {
+  const [user, setUser] = useState({})
+  
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
  const handleGoogleSignIn= ()=>{
-  const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider)
+  
+
+  signInWithPopup(auth, googleProvider)
   .then((result) =>{
     const user = result.user;
-    console.log(user)
+    setUser(user)
+    console.log(user)  
   })
   .catch((error)=>{
     console.log('error', error)
   })
  }
-  
+
+ const handleGitHubSignIn=()=>{
+  signInWithPopup(auth, gitHubProvider)
+  .then((result)=>{
+    const user = result.user;
+    setUser(user)
+    console.log(user)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+ }
+
+ const handleSignOut=()=>{
+  signOut(auth)
+  .then(()=>{
+    setUser({})
+  })
+  .catch((error)=>{
+    setUser({})
+  })
+ } 
   
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        user.email? <button onClick={handleSignOut}>Sign Out</button>:   
+        <div>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleGitHubSignIn}> Github Sign In</button>
+
+        </div>
+      }
+    
+      
+      <h1>You Name:{user.displayName}</h1>
+      <h1>You Email Address:{user.email}</h1>
     </div>
   );
 }
